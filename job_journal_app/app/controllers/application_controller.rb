eed85b98e091cb3d67229106242a370ec8df5409
DKendle <dina.kendle@gmail.com>
2020-07-06 12:25:24 -0400

    Completets GET signup route

--- job_journal_app/app/controllers/application_controller.rb
+++ job_journal_app/app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get '/' do
@@ -12,10 +14,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do 
-    erb :signup
+    erb :'users/signup'
   end
 
   post '/signup' do 
-    user = User.new()
+    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if user.save
+      session[:id] = user.id
+    else
+      redirect "/signup"
+    end
   end
+
 end
--- job_journal_app/config.ru
+++ job_journal_app/config.ru
@@ -4,5 +4,6 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
-use 
+use PostController
+use UserController
 run ApplicationController
