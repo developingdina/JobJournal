2ea8c422e76eb95287300849a39dd5d50340f072
DKendle <dina.kendle@gmail.com>
2020-07-10 16:06:49 -0400

    Adds sinatra flash messages and errors for signup view

--- job_journal_app/app/controllers/application_controller.rb
+++ job_journal_app/app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 require './config/environment'
-
+require 'sinatra/flash'
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+  
+    register Sinatra::Flash 
+  
     set :session_secret, ENV['SESSION_PASSWORD']
   end
 
--- job_journal_app/app/controllers/user_controller.rb
+++ job_journal_app/app/controllers/user_controller.rb
@@ -11,16 +11,18 @@ class UserController < ApplicationController
     
     post '/signup' do 
         if User.find_by(username: params[:username])
-            "Sorry that username already exists! Try again"
-            #redirect "/signup"
+            flash[:errors] = "Sorry that username already exists! Try again:"
+            redirect "/signup"
         else
-                user = User.new(username: params[:username], email: params[:email], password: params[:password])
+            user = User.new(username: params[:username], email: params[:email], password: params[:password])
     
             if user.save 
                 session[:user_id] = user.id 
             else
+                flash[:errors] = "Sorry about that. It's not you, its' us. Try again"
                 redirect "/signup"
             end
+        flash[:message] = "Welcome #{user.username}! You were successfully logged in."
         redirect "/posts"
         end
     end
@@ -32,7 +34,7 @@ class UserController < ApplicationController
             erb :'users/login'
         end
     end
-#########################
+
     post '/login' do 
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
@@ -42,7 +44,7 @@ class UserController < ApplicationController
             redirect "/login"
         end
     end
-#########################
+
     get '/logout' do
         if logged_in?
             session.clear
