b140461507f2c2816a27832f661e8a40ad107695
DKendle <dina.kendle@gmail.com>
2020-07-20 16:52:36 -0400

    Reformats patch request to update attributes based on params values

--- job_journal_app/app/controllers/post_controller.rb
+++ job_journal_app/app/controllers/post_controller.rb
@@ -17,7 +17,6 @@ class PostController < ApplicationController
 
     post '/posts' do 
         if logged_in?
-
             post = current_user?.posts.new(params)
             post.save
             flash[:message] = "Post successfully created!"
@@ -28,7 +27,7 @@ class PostController < ApplicationController
        
     end
 
-##added new helper method and got rid of 
+##added new helper method and got rid of nested if statement
     get '/posts/:id' do
         if post_by_user
             erb :'posts/show_post'
@@ -44,18 +43,15 @@ class PostController < ApplicationController
     end
 
     patch '/posts/:id' do 
-        post = current_user?.posts.find_by_id(params[:id])
-        post.company_name = params[:companyname]
-        post.position_title = params[:positiontitle]
-        post.description = params[:description]
-        post.applied = params[:dateapplied]
-        post.save
+        post_by_user
+        @post.update_attributes(company_name: params[:company_name], position_title: params[:position_title], applied: params[:applied], description: params[:description])
+        @post.save
         flash[:message] = "Post successfully updated!"
-        redirect "/posts/#{post.id}"
+        redirect "/posts/#{@post.id}"
     end
 
     delete '/posts/:id' do
-        @post = current_user?.posts.find_by_id(params[:id])
+        post_by_user
         @post.delete 
         flash[:message] = "Post successfully deleted!"
         redirect "/posts"
